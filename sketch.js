006c17b791e5eb7bde5a1b5e4a5d79e4b5742654
hkrai9218 <hkrai9218@gmail.com>
2022-05-22 19:26:44 -0700

    Updated sketch

--- sketch.js
+++ sketch.js
@@ -1,23 +1,8 @@
-/*
-Game State Example - Art 101 - Spring 2022 - Chelsea Thompto
-
-This example shows a basic structure for moving between game states/stages
-by using a variable called 'gameState' and the JavaScript 'switch'
-functionality in the draw loop. Depending on the which on how gameState is
-defined, different displays and user interactions are active. This is
-accomplished by placing these elements into functions and calling those
-functions within the corresponding 'case' inside the draw loop.
-
-A more advanced version of this program can be viewed here:
-https://github.com/Chelsea-Thompto-Teaching-Examples/art101-game-state-demo
-*/
-
-// Define the gameState variable & set it to the desired inital value.
 let gameState = 'title';
 let canvas;
-let bgR = 100;
-let bgG = 240;
-let bgB = 100;
+let bgR = 233;
+let bgG = 208;
+let bgB = 223;
 
 // Set up
 function setup() {
@@ -26,111 +11,112 @@ function setup() {
   frameRate(60);
 }
 
-/* The draw loop content is drawn depending on the current value of
-gameState. The 'switch' function here is replacing what could be an
-'if-else' statement. */
 function draw() {
 
-  // The switch call expects to find a varible within the parentheses.
   switch (gameState) {
 
-    /*
-    Each 'screen' that you want should be defined with a word, this word
-    will correspond to a 'case' as seen below. The case will be followed by
-    all of functions you want within that screen and end with a 'break;'.
-    */
     case 'title':
       titleScreen();
       break;
     case 'lvl1':
       gameStage1();
       break;
-    case 'gameover':
-      gameOver();
+    case 'lvl2':
+      gameStage2();
       break;
-    /*
-    To add more screens, you will need to:
-    1) define a new case value to be added to your switch function
-    2) create a new function to be called for the case
-    3) create an event within your code to define gameState as the new
-    case.
-
-    Example: For an instructions screen, I might:
-    1) Add a case for gameStage being defined as 'instructions'
-    2) creation a function called instDis() to house my instruction screen
-    graphics
-    3) create a keyboard event to switch gameState to 'instructions' when a
-    certain key is pressed on the title screen
-    */
+    case 'lvl3':
+      gameState3();
+      break;
+    case 'lvl4':
+      gameState4();
+      break;
   }
 }
 
-/* Key release listener for starting game while on the title and game over
-screens and changing the background color on the main game screen. Here we
-can see how the variable 'gameState' gets changed to trigger a new screen
-to be displayed. */
 function keyReleased() {
-  if (gameState === 'title' || gameState === 'gameover') {
-    if (key === 's' || key === 'S' ) {
+  if (gameState === 'title') {
+    if (key === 'w' || key === 'W' ) {
       gameState = 'lvl1';
-      bgR = 100;
-      bgG = 240;
-      bgB = 100;
+      bgR = 233;
+      bgG = 208;
+      bgB = 223;
     }
   } else if (gameState === 'lvl1') {
-    if (key === 's' || key === 'S' ) {
-      bgR = random(255);
-      bgG = random(255);
-      bgB = random(255);
+    if (key === 'w' || key === 'W' ) {
+      gameState = 'lvl2';
+      frameRate(30);
+      textSize(30);
+      textAlign(CENTER);
+    }
+  }
+  else if (gameState === 'lvl2') {
+    if (key === 'w' || key === 'W') {
+      gameState = 'lvl3';
+    }
+  }
+  else if (gameState === 'lvl3') {
+    if (key === 'w' || key === 'W') {
+      gameState = 'lvl4';
+    }
+  }
+  else if (gameState === 'lvl4') {
+    if (key === 'w' || key === 'W') {
+      gameState = 'title';
     }
   }
 }
 
-// Function for rendering the title screen.
 function titleScreen() {
-  background(100, 100, 240);
+  background(233, 208, 223);
   stroke(255);
   fill(255);
-  textSize(75);
+  textSize(50);
   textAlign(CENTER);
-  text('GAY', width*0.5, height*0.33);
+  text('Bay Area', width*0.5, height*0.33);
+  text('and the usage', width*0.5, height*0.5);
+  text('of phones', width*0.5, height*0.65);
   textSize(25);
-  text('Press "S" To Start Game', width*0.5, height*0.66);
+  text('To continue press "W"', width*0.5, height*0.95);
 }
 
-// Function for rendering the main game play screen.
 function gameStage1() {
   background(bgR, bgG, bgB);
   stroke(0);
   fill(0);
-  textSize(50);
-  textAlign(CENTER);
-  text('CIRCLE!', width*0.5, height*0.1);
   textSize(20);
-  text('Press "S" to change background color.', width*0.5, height*0.95);
-  ellipseMode(CENTER);
-  ellipse(width/2, height/2, 40);
-  stroke(255);
-  fill(255);
-  rectMode(CENTER);
-  rect(mouseX, mouseY, 20);
-
-  // Checking for lose state (touching circle).
-  if (mouseX > (width/2)-20 && mouseX < (width/2)+20) {
-    if (mouseY > (height/2)-20 && mouseY < (height/2)+20) {
-      gameState = 'gameover';
-    }
-  }
+  textAlign(CENTER);
+  text('The first iPhone was in development in 2007', width*0.5, height*0.1);
+  text('To continue press "W"', width*0.5, height*0.95);
 }
 
-// Function for rendering game over screen.
-function gameOver() {
-  background(240, 0 ,0);
-  stroke(255);
-  fill(255);
-  textSize(75);
-  textAlign(CENTER);
-  text('OOP', width*0.5, height*0.33);
-  textSize(25);
-  text('Press "S" To Restart Game', width*0.5, height*0.66);
+function gameStage2() {
+  background(220, 208, 223);
+  stroke(0);
+  fill(0);
+  textSize(20);
+  text('The time between the first iPhone and now.', width*0.5, height*0.8);
+  text(frameCount, width / 2, height / 2);
+  text('To continue press "W"', width*0.5, height*0.95);
+}
+
+function gameState3() {
+  background(200, 100, 150);
+  stroke(0);
+  fill(0);
+  textSize(20);
+  text('The amount of phones purchased each year staggers.', width*0.5, height*0.4);
+  text('There is an x amount of', width*0.5, height*0.5);
+  text('phones being purchased every two minutes.', width*0.5, height*0.6);
+  text('To continue press "W"', width*0.5, height*0.95);
+}
+
+function gameState4() {
+  background(240, 260, 200);
+  stroke(0);
+  fill(0);
+  textSize(20);
+  text('The numbers continue to increase.', width*0.5, height*0.3);
+  text('In the Bay Area, an X amount of people', width*0.5, height*0.5)
+  text('have more than one Phone on their person.', width*0.5, height*0.7);
+  text('To restart press "W"', width*0.5, height*0.95);
 }
