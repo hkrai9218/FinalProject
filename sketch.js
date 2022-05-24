def662b5353b126012e4ec6bbcbb1da89deb747e
hkrai9218 <hkrai9218@gmail.com>
2022-05-23 17:06:26 -0700

    Updated sketch

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ let bgB = 223;
 
 // Set up
 function setup() {
-  canvas = createCanvas(500, 500);
+  canvas = createCanvas(600, 500);
   canvas.parent('sketch-holder');
   frameRate(60);
 }
@@ -68,7 +68,10 @@ function keyReleased() {
 
 function titleScreen() {
   background(233, 208, 223);
-  fill(240, 210, 230);
+  fill(210, 180, 200);
+  noStroke();
+  rect(width*0.0, height*0.0, width*0.05, height*0.99);
+  rect(width*0.95, height*0.0, width*0.99, height*0.99);
   rect(width*0, height*0, width*99, height*0.05);
   rect(width*0, height*0.85, width*99, height*0.99);
   stroke(255);
@@ -84,20 +87,35 @@ function titleScreen() {
 
 function gameStage1() {
   background(bgR, bgG, bgB);
+  fill(210, 180, 200);
+  noStroke();
+  rect(width*0.0, height*0.0, width*0.05, height*0.99);
+  rect(width*0.95, height*0.0, width*0.99, height*0.99);
+  rect(width*0, height*0, width*99, height*0.05);
+  rect(width*0, height*0.85, width*99, height*0.99);
   stroke(0);
   fill(0);
   textSize(20);
   textAlign(CENTER);
   text('The first iPhone was in development in 2007', width*0.5, height*0.1);
+  text('Fun Fact: Apple faced two lawsuits', width*0.5, height*0.15);
+  text('regarding selling iPhones.', width*0.5, height*0.2);
   text('To continue press "W"', width*0.5, height*0.95);
 }
 
 function gameStage2() {
   background(220, 208, 223);
+  fill(200, 170, 200);
+  noStroke();
+  rect(width*0.0, height*0.0, width*0.05, height*0.99);
+  rect(width*0.95, height*0.0, width*0.99, height*0.99);
+  rect(width*0, height*0, width*99, height*0.05);
+  rect(width*0, height*0.9, width*99, height*0.99);
   stroke(0);
   fill(0);
   textSize(20);
-  text('The time between the first iPhone and now.', width*0.5, height*0.8);
+  text('How many phones do you think were bought.', width*0.5, height*0.8);
+  text('after 2008?', width*0.5, height*0.85);
   textSize(150);
   text(frameCount, width / 2, height / 2);
   textSize(20);
@@ -106,6 +124,12 @@ function gameStage2() {
 
 function gameState3() {
   background(200, 100, 150);
+  fill(195, 155, 170);
+  noStroke();
+  rect(width*0.0, height*0.0, width*0.05, height*0.99);
+  rect(width*0.95, height*0.0, width*0.99, height*0.99);
+  rect(width*0, height*0, width*99, height*0.05);
+  rect(width*0, height*0.85, width*99, height*0.99);
   stroke(0);
   fill(0);
   textSize(20);
@@ -116,7 +140,13 @@ function gameState3() {
 }
 
 function gameState4() {
-  background(240, 260, 200);
+  background(240, 250, 200);
+  fill(210, 220, 150);
+  noStroke();
+  rect(width*0.0, height*0.0, width*0.05, height*0.99);
+  rect(width*0.95, height*0.0, width*0.99, height*0.99);
+  rect(width*0, height*0, width*99, height*0.05);
+  rect(width*0, height*0.85, width*99, height*0.99);
   stroke(0);
   fill(0);
   textSize(20);
